5505796863011db6ab00e624ce63dd5bd7d25bec
Anton <toni89_github@bra1n.de>
2015-02-22 19:48:50 +0100

    minor mobile fixes

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -34,7 +34,7 @@ modules.mongojs = mongojs;
 modules.mailer = nodemailer.createTransport(smtpTransport({
     host: config.email.host,
     port: config.email.port,
-    secure: false,
+    secure: true,
     debug: config.email.debug,
     auth: {
         user: config.email.user,
